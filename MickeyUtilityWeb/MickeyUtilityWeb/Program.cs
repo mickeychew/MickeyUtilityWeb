05bb6440973291522eeb4258b1a4404c291040a3
chuchiang91 <pandachew91@gmail.com>
2024-07-25 01:30:03 +0800

    redeploy

--- MickeyUtilityWeb/MickeyUtilityWeb/Program.cs
+++ MickeyUtilityWeb/MickeyUtilityWeb/Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+//deployment
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);

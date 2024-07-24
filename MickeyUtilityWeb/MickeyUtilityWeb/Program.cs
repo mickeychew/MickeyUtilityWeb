d8f48b4d85dbb9958a8e3b720755edaf1c615250
chuchiang91 <pandachew91@gmail.com>
2024-07-25 01:39:17 +0800

    fix login

--- MickeyUtilityWeb/MickeyUtilityWeb/Program.cs
+++ MickeyUtilityWeb/MickeyUtilityWeb/Program.cs
@@ -9,14 +9,22 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-//deployment
+
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/Files.Read.All");
 
     // Set the redirect URI dynamically based on the environment
-    options.ProviderOptions.Authentication.RedirectUri = $"{builder.HostEnvironment.BaseAddress}authentication/login-callback";
+    var baseUri = builder.HostEnvironment.BaseAddress.TrimEnd('/');
+    if (baseUri.Contains("github.io"))
+    {
+        options.ProviderOptions.Authentication.RedirectUri = $"{baseUri}/authentication/login-callback";
+    }
+    else
+    {
+        options.ProviderOptions.Authentication.RedirectUri = $"{baseUri}/authentication/login-callback";
+    }
 });
 
 builder.Services.AddScoped<SGItineraryService>();

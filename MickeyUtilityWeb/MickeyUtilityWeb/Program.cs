c9030d0db3b713f96b51d2c60cb805303c2cd5a1
chuchiang91 <pandachew91@gmail.com>
2024-07-25 10:31:45 +0800

    fix deployment

--- MickeyUtilityWeb/MickeyUtilityWeb/Program.cs
+++ MickeyUtilityWeb/MickeyUtilityWeb/Program.cs
@@ -1,14 +1,20 @@
+using MickeyUtilityWeb.Services;
+using MickeyUtilityWeb;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MickeyUtilityWeb;
-using MickeyUtilityWeb.Services;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Determine the base address
+var baseAddress = builder.HostEnvironment.BaseAddress;
+if (baseAddress.Contains("github.io"))
+{
+    baseAddress = "https://mickeychew.github.io/MickeyUtilityWeb/";
+}
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
 
 builder.Services.AddMsalAuthentication(options =>
 {
@@ -20,20 +26,8 @@ builder.Services.AddMsalAuthentication(options =>
     options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/Sites.ReadWrite.All");
     options.ProviderOptions.LoginMode = "redirect";
 
-    // Set the correct redirect URI
-    var baseUri = builder.HostEnvironment.BaseAddress.TrimEnd('/');
-    var githubPagesUri = "https://mickeychew.github.io/MickeyUtilityWeb";
-
-    if (baseUri.StartsWith(githubPagesUri))
-    {
-        options.ProviderOptions.Authentication.RedirectUri = $"{githubPagesUri}/authentication/login-callback";
-        options.ProviderOptions.Authentication.PostLogoutRedirectUri = githubPagesUri;
-    }
-    else
-    {
-        options.ProviderOptions.Authentication.RedirectUri = $"{baseUri}/authentication/login-callback";
-        options.ProviderOptions.Authentication.PostLogoutRedirectUri = baseUri;
-    }
+    options.ProviderOptions.Authentication.RedirectUri = $"{baseAddress}authentication/login-callback";
+    options.ProviderOptions.Authentication.PostLogoutRedirectUri = baseAddress;
 });
 
 builder.Services.AddScoped<SGItineraryService>();

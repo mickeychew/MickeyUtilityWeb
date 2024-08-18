4779ed1d77c686fdf2bae4ef429e32769fb7f625
chuchiang91 <pandachew91@gmail.com>
2024-08-18 11:58:17 +0800

    change to secrets

--- MickeyUtilityWeb/MickeyUtilityWeb/Services/FieldServices/FileIdService.cs
+++ MickeyUtilityWeb/MickeyUtilityWeb/Services/FieldServices/FileIdService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using OfficeOpenXml;
 
 namespace MickeyUtilityWeb.Services
@@ -11,15 +12,17 @@ namespace MickeyUtilityWeb.Services
     {
         private readonly ExcelApiService _excelApiService;
         private readonly ILogger<FileIdService> _logger;
-        private const string MASTER_FILE_ID = "85E9FC7E76F38D5C!sc5657dc664e7429eaeff80d2ace5c55d";
+        private readonly IConfiguration _configuration;
+        private readonly string _masterFileId;
         private const string WORKSHEET_NAME = "Sheet1";
-
         private Dictionary<string, string> _fileIdCache;
 
-        public FileIdService(ExcelApiService excelApiService, ILogger<FileIdService> logger)
+        public FileIdService(ExcelApiService excelApiService, ILogger<FileIdService> logger, IConfiguration configuration)
         {
             _excelApiService = excelApiService;
             _logger = logger;
+            _configuration = configuration;
+            _masterFileId = _configuration["MasterFileId"];
             _fileIdCache = new Dictionary<string, string>();
         }
 
@@ -29,14 +32,11 @@ namespace MickeyUtilityWeb.Services
             {
                 return fileId;
             }
-
             await RefreshFileIdCache();
-
             if (_fileIdCache.TryGetValue(key, out fileId))
             {
                 return fileId;
             }
-
             throw new KeyNotFoundException($"File ID for key '{key}' not found.");
         }
 
@@ -44,28 +44,28 @@ namespace MickeyUtilityWeb.Services
         {
             try
             {
-                var excelContent = await _excelApiService.GetFileContent(MASTER_FILE_ID);
+                if (string.IsNullOrEmpty(_masterFileId))
+                {
+                    throw new InvalidOperationException("MasterFileId is not set in the configuration.");
+                }
 
+                var excelContent = await _excelApiService.GetFileContent(_masterFileId);
                 using (var stream = new MemoryStream(excelContent))
                 using (var package = new ExcelPackage(stream))
                 {
                     var worksheet = package.Workbook.Worksheets[WORKSHEET_NAME];
                     var rowCount = worksheet.Dimension.Rows;
-
                     _fileIdCache.Clear();
-
                     for (int row = 2; row <= rowCount; row++)
                     {
                         var key = worksheet.Cells[row, 1].Value?.ToString();
                         var value = worksheet.Cells[row, 2].Value?.ToString();
-
                         if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(value))
                         {
                             _fileIdCache[key] = value;
                         }
                     }
                 }
-
                 _logger.LogInformation("File ID cache refreshed successfully");
             }
             catch (Exception ex)
